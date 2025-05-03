4c8cf8d7730d0fe7488cc2291aaa28420cccd22e
mati <mati@mati.com>
2025-05-03 17:00:36 +0200

    packages helper script

---
+++ packages.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+packages=(
+    "stow"
+    "nvim"
+    "tmux"
+    "xclip"
+    "go"
+    "fzf"
+    "neofetch"
+    "htop"
+    "rg"
+)
+
+for package in "${packages[@]}"; do
+    if command -v $package >/dev/null 2>&1; then
+        echo -e "\e[32m\e[0m $package"
+    else
+        echo -e "\e[31m\e[0m $package"
+    fi
+done
